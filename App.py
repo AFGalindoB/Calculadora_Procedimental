bb5e837228179c06c47aee9e1801161021b0b283
AFGalindoB <andresgalindowow1226@gmail.com>
2024-09-18 01:44:34 -0500

    Se documentan las nuevas funciones, se reorganizan y se crean botones

--- App.py
+++ App.py
@@ -57,57 +57,78 @@ class Calculadora:
         
         #Ejecuta funciones de los botones C, DEL, +/-
         def execute_functions(index):
+            #Borra lo que hay en la entrada
             if index == "C":
                 entry.config(text="0")
+                
+            #Borra el ultimo digito o simbolo escrito
             elif index == "DEL":
-                text = entry.cget("text")
+                text = entry.cget("text") #Obtenemos el texto de la entrada
+                
                 if len(text) > 1:
+                    #Si el tamaño del texto es mayor a 1 eliminamos el ultimo caracter
                     text = text[:-1]
                     entry.config(text=text)
                 else:
-                    entry.config(text="0")
+                    entry.config(text="0") #Si es menor o igual a 1 se cambia el texto por un 0
+            
+            #Cambia el signo del ultimo valor escrito
             elif index == "+/-":
-                expresion = find(r'-?\d*\.\d+|-?\d+|\+|\-|\*|\/|\(|\)',entry.cget("text"))
+                expresion = find(r'-?\d*\.\d+|-?\d+|\+|\-|\*|\/|\(|\)',entry.cget("text")) #Obtiene la expresion como una lista
                 try:
                     valor = self.convertir_valor(expresion[-1])
+                    #Si el tamaño de la expresion es de 1 solo se cambia el valor que esta alli
                     if len(expresion) == 1:
                         expresion[0] = str(valor*-1)
                     else:
+                        #Si el valor es menor a 0 cambiamos el valor a positivo y agregamos un '+' antes del valor
                         if valor < 0 and expresion[-2] != "*" and expresion[-2] != "/":
                             expresion[-1] = "+"+str(valor*-1)
                         else:
+                            #Si la penultima posicion es un '+' eliminamos ese '+' y hacemos el cambio de signo de valor
                             if expresion[-2] == "+":
                                 expresion.pop(-2)
                                 expresion[-1] = str(valor*-1)
                             else:
                                 expresion[-1] = str(valor*-1)
-                    entry.config(text="".join(map(str,expresion)))
-                except:
-                    pass
+                    
+                    entry.config(text="".join(map(str,expresion))) #Actualizamos la entrada usando la lista expresion
+                    
+                except: pass #Si el except se ejecuta es debido a que la ultima posicion es un simbolo por ende no se hace nada
         
+            elif index == ".":
+                update_label(index)
+        #Ejecuta funciones de los botones M+, M-, MC, MR
         def execute_memory(index):
             if index == "M+" or index == "M-":
-                entrada = find(r'-?\d*\.\d+|-?\d+|\+|\-|\*|\/|\(|\)',entry.cget("text"))
+                entrada = find(r'-?\d*\.\d+|-?\d+|\+|\-|\*|\/|\(|\)',entry.cget("text")) #Obtiene la expresion como una lista
                 if self.memory == None:
                     try: self.memory = self.convertir_valor(entrada[0] if len(entrada) == 1 else entrada[-1])
-                    except: pass
+                    except: pass #Si se ejecuta es porque la entrada es un simbolo
                 else:
                     try:
                         valor = self.convertir_valor(entrada[0] if len(entrada) == 1 else entrada[-1])
-                        self.memory = self.memory + valor if index == "M+" else self.memory - valor
-                    except: pass
+                        self.memory = self.memory + valor if index == "M+" else self.memory - valor #Sumamos o restamos memory con valor
+                    except: pass #Si se ejecuta es porque la entrada es un simbolo
             elif index == "MC":
                 self.memory = None
             elif index == "MR":
                 if self.memory != None:
                     entrada = find(r'-?\d*\.\d+|-?\d+|\+|\-|\*|\/|\(|\)',entry.cget("text"))
                     if len(entrada) == 1:
-                        entry.config(text=str(self.memory))
+                        entry.config(text=str(self.memory)) # cambiamos la entrada por lo que hay en memoria
                     else:
-                        entrada[-1] = self.memory
-                        entry.config(text="".join(map(str,entrada)))
-        
-        #Creamos una funcion para acutalizar el label
+                        #Comprobamos que la ultima posicion no sea un simbolo
+                        if entrada[-1] != "+" and entrada[-1] != "-" and entrada[-1] != "*" and entrada[-1] != "/":
+                            #Si no hay simbolos se cambia la ultima posicion por memory
+                            entrada[-1] = self.memory
+                        else:
+                            #Si hay un simbolo agregamos al final de la lista memory
+                            entrada.append(self.memory)
+                            
+                        entry.config(text="".join(map(str,entrada))) #Actualizamos la entrada
+     
+        #Creamos una funcion para acutalizar la entrada
         def update_label(index, Evaluar:str=False):
             if Evaluar:
                 result = self.evaluar(Exp=entry.cget("text"),Tipo="EE")
@@ -115,14 +136,17 @@ class Calculadora:
             else:
                 actualtxt = entry.cget("text")
                 if index != ".":
-                    entry.config(text=index) if actualtxt == "0" or actualtxt == "Error" or actualtxt == "SyntaxError" else entry.config(text=actualtxt + index)
+                    if actualtxt == "0" and index == "00":
+                        pass
+                    else:
+                        entry.config(text=index) if actualtxt == "0" or actualtxt == "Error" or actualtxt == "SyntaxError" else entry.config(text=actualtxt + index)
                 else:
                     entry.config(text=actualtxt + index)
         
         #Definimos los botones
         memo = ["M+","M-","MR","MC"]
-        functions = ["C","DEL","+/-"]
-        nums = ["7","8","9","4","5","6","1","2","3","0","."]
+        functions = ["PRO","C","DEL","+/-","."]
+        nums = ["7","8","9","4","5","6","1","2","3","0","00"]
         symbols = ["/","*","-","+"]
         
         #Boton del igual para evaluar la expresion
@@ -135,7 +159,7 @@ class Calculadora:
             memo_buttons = tk.Button(calc,text=memo[i],command=partial(execute_memory,memo[i]),**button_properties)
             memo_buttons.grid(row=1,column=col)
         for i in range(len(functions)):
-            row = (i % 4) + 2
+            row = (i % 5) + 1
             function_buttons = tk.Button(calc,text=functions[i],command=partial(execute_functions,functions[i]),**button_properties)
             function_buttons.grid(row=row,column=0)
         for i in range(len(nums)):
